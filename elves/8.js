660456e6623b5c3aeaa502b0afdf844b13fb82c2
Samuel Robinson <pinkstarsr11@gmail.com>
2019-01-18 23:07:56 -0800

    made boney elf microscopically more easy

--- elves/8.js
+++ elves/8.js
@@ -102,7 +102,7 @@ addMove({
         } else {
             user.state.alchoholWarning = true;
             return {
-                text: `but ${user.name} can't get any more lit`
+                text: `but can ${user.name} can get any more lit?`
             }
         }
     }
