ebfc94e826a19e2cbadfc9967e63ba0d48d9e472
Samuel Robinson <pinkstarsr11@gmail.com>
2019-02-07 05:35:10 -0800

    Added collision to o n e

--- elven-one/renderers/overworld-test.js
+++ elven-one/renderers/overworld-test.js
@@ -8,7 +8,7 @@ function OverworldTestRenderer() {
 
     const tileRolloverTime = 1000;
 
-    let playerY = 0, playerX = halfWidth;
+    let playerY = 50, playerX = halfWidth;
     let jumpOffset = 0;
 
     const changeTimeFactor = 1000;
@@ -20,7 +20,6 @@ function OverworldTestRenderer() {
 
     const walkStartDelay = 0;
     let walkStartTime = null;
-
 
     this.playerAnimator.SetLegsMoving();
     this.playerAnimator.SetWalkingSpeed(1);
@@ -40,6 +39,8 @@ function OverworldTestRenderer() {
 
     let leftMovementBlocked = false;
     let rightMovementBlocked = false;
+    let upMovementBlocked = false;
+    let downMovementBlocked = false;
 
     let lastBulletTime = 0;
     const bulletDelay = 1;
@@ -50,6 +51,26 @@ function OverworldTestRenderer() {
     let playerCrouched = false;
 
     const playerScale = 20;
+
+    const playerCollisionWidth = 5 * playerScale;
+    const playerCollisionHeight = 18 * playerScale;
+    const halfPlayerCollisionWidth = playerCollisionWidth/2;
+    const halfPlayerCollisionHeight = playerCollisionHeight/2;
+
+    const collisionObjects = [
+        null,{
+            x: halfWidth,
+            y: fullHeight - 500,
+            width: 50,
+            height: 50,
+        },
+        {
+            x: halfWidth,
+            y: fullHeight - 50,
+            width: 50,
+            height: 50,
+        }
+    ];
 
     const gameTick = () => {
         if(!playerJumping) {
@@ -158,6 +179,61 @@ function OverworldTestRenderer() {
             playerMoving = false;
             this.playerAnimator.SetLegsIdle();
         }
+
+    }
+    const getPlayerCollisionState = () => {
+        const collision = {
+            up:false,
+            down:false,
+            left:false,
+            right:false,
+
+            upPos: null,
+            downPos: null,
+            leftPos:null,
+            rightPos:null,
+        }
+
+        const playerCrouchDifference = playerCrouched && !playerJumping ? playerScale * 3 : 0;
+        const collisionHeight = playerCollisionHeight - playerCrouchDifference;
+        const playerCollisionYStart = fullHeight-playerCollisionHeight-(playerY+jumpOffset)+playerCrouchDifference;
+        const playerCollisionYCenter = playerCollisionYStart + (collisionHeight / 2);
+
+        let collisionIndex = collisionObjects.length;
+
+        while(--collisionIndex) {
+            const collisionItem = collisionObjects[collisionIndex];
+
+            const w = 0.5 * (playerCollisionWidth + collisionItem.width);
+            const h = 0.5 * (collisionHeight + collisionItem.height);
+            const dx = playerX - (collisionItem.x + (collisionItem.width/2));
+            const dy = playerCollisionYCenter - (collisionItem.y + (collisionItem.height/2));
+
+            if (Math.abs(dx) <= w && Math.abs(dy) <= h) {
+
+                const wy = w * dy;
+                const hx = h * dx;
+                if (wy > hx) {
+                    if (wy > -hx) {
+                        collision.up = true;
+                        collision.upPos = collisionItem.y + collisionItem.height;
+                    } else {
+                        collision.right = true;
+                        collision.rightPos = collisionItem.x;
+                    }
+                } else {
+                    if (wy > -hx) {
+                        collision.left = true;
+                        collision.leftPos = collisionItem.x + collisionItem.width;
+                    } else {
+                        collision.down = true;
+                        collision.downPos = collisionItem.y;
+                    }
+                }
+            }
+
+        }
+        return collision;
     }
 
     const setKey = (key,pressed) => {
@@ -194,6 +270,7 @@ function OverworldTestRenderer() {
         }
     }
 
+
     this.processKey = key => setKey(key,true);
     this.processKeyUp = key => setKey(key,false);
 
@@ -204,6 +281,88 @@ function OverworldTestRenderer() {
     }
 
     let lastFrame = 0;
+
+    this.showHitBox = false;
+
+    const updateJumpPosition = delta => {
+        const newPos = jumpOffset + delta;
+        const collisionState = getPlayerCollisionState();
+        if(jumpPhase > 0) {
+            if(!collisionState.up) {
+                jumpOffset = newPos;
+                return true;
+            } else {
+                const upPosDifference = fullHeight - collisionState.upPos
+                if(jumpOffset + playerY + playerCollisionHeight > upPosDifference) {
+                    jumpOffset = (upPosDifference - playerCollisionHeight) - playerY;
+                }
+                return false;
+            }
+        } else if(jumpPhase < 0) {
+            if(!collisionState.down) {
+                jumpOffset = newPos;
+                return true;
+            } else {
+                const downPosDifference = fullHeight - collisionState.downPos;
+                if(jumpOffset + playerY < downPosDifference) {
+                    jumpOffset = downPosDifference - playerY;
+                }
+                return false;
+            }
+        }
+        return false;
+    }
+    const updatePlayerPositionX = delta => {
+        const newPos = playerX + delta;
+        const collisionState = getPlayerCollisionState();
+        if(delta > 0) {
+            if(!collisionState.right) {
+                playerX = newPos;
+                this.playerAnimator.SetLegsMoving();
+            } else {
+                if(playerX + halfPlayerCollisionWidth > collisionState.rightPos) {
+                    playerX = collisionState.rightPos - halfPlayerCollisionWidth;
+                }
+                this.playerAnimator.SetLegsIdle();
+            }
+        } else if(delta < 0) {
+            if(!collisionState.left) {
+                playerX = newPos;
+                this.playerAnimator.SetLegsMoving();
+            } else {
+                if(playerX - halfPlayerCollisionWidth < collisionState.leftPos) {
+                    playerX = collisionState.leftPos +  halfPlayerCollisionWidth;
+                }
+                this.playerAnimator.SetLegsIdle();
+            }
+        }
+    }
+    const updatePlayerPositionY = delta => {
+        const newPos = playerY + delta;
+        const collisionState = getPlayerCollisionState();
+        if(delta > 0) {
+            if(!collisionState.up) {
+                playerY = newPos;
+            } else {
+                const upPosDifference = fullHeight - collisionState.upPos
+                if(jumpOffset + playerY + playerCollisionHeight > upPosDifference) {
+                    playerY = (upPosDifference - playerCollisionHeight) - jumpOffset;
+                }
+            }
+        } else if(delta < 0) {
+            if(!collisionState.down) {
+                playerY = newPos;
+            } else {
+                const downPosDifference = fullHeight - collisionState.downPos;
+                if(jumpOffset + playerY < downPosDifference) {
+                    playerY = downPosDifference - jumpOffset;
+                }
+            }
+        }
+    }
+    const updateBulletPositionX = (bullet,delta) => {
+        bullet.positionX += delta;
+    }
 
     this.render = timestamp => {
 
@@ -221,34 +380,48 @@ function OverworldTestRenderer() {
         }
 
         if(playerJumping) {
-            if(jumpPhase > 0 && !rightMovementBlocked) {
-                jumpOffset += (timeDelta / changeTimeFactor) * jumpChange;
-                if(jumpOffset >= jumpMaxHeight) {
-                    jumpOffset = jumpMaxHeight;
+            if(jumpPhase > 0) {
+                if(!upMovementBlocked) {
+                    if(!updateJumpPosition((timeDelta / changeTimeFactor) * jumpChange)) {
+                        jumpPhase = -1;
+                    } else if(jumpOffset >= jumpMaxHeight) {
+                        jumpOffset = jumpMaxHeight;
+                        jumpPhase = -1;
+                    }
+                } else {
                     jumpPhase = -1;
                 }
             } else {
-                jumpOffset -= (timeDelta / changeTimeFactor) * jumpChange;
-                if(jumpOffset <= 0 && !leftMovementBlocked) {
+                if(!updateJumpPosition(-(timeDelta / changeTimeFactor) * jumpChange)) {
+                    playerY = jumpOffset;
+                    jumpOffset = 0;
+                    playerJumping = false;
+                    this.playerAnimator.SetOnLand();
+                } else if(jumpOffset <= 0) {
                     jumpOffset = 0;
                     playerJumping = false;
                     this.playerAnimator.SetOnLand();
                 }
             }
+        } else if(playerY > 0) {
+            updatePlayerPositionY(-((timeDelta / changeTimeFactor) * jumpChange));
+            if(playerY < 0) {
+                playerY = 0;
+            }
         }
         if(playerMoving && timestamp >= walkStartDelay + walkStartTime) {
             this.playerAnimator.SetLegsMoving();
             if(playerVelocity > 0) {
-                playerX += (timeDelta / changeTimeFactor) * playerVelocityChange;
+                updatePlayerPositionX((timeDelta / changeTimeFactor) * playerVelocityChange);
             } else {
-                playerX -= (timeDelta / changeTimeFactor) * playerVelocityChange;
+                updatePlayerPositionX(-((timeDelta / changeTimeFactor) * playerVelocityChange));
             }
         }
         let bulletIndex = bullets.length;
         let bulletChange = (timeDelta / changeTimeFactor) * bulletVelocityChange;
 
         while(--bulletIndex) {
-            bullets[bulletIndex].positionX += bullets[bulletIndex].isRight ? bulletChange : - bulletChange;
+            updateBulletPositionX(bullets[bulletIndex],bullets[bulletIndex].isRight ? bulletChange : - bulletChange);
 
             context.fillStyle = "white";
 
@@ -257,6 +430,16 @@ function OverworldTestRenderer() {
             if(Math.abs(bullets[bulletIndex].positionX) > 2000) {
                 bullets.splice(bulletIndex,1);
             }
+        }
+
+        context.fillStyle = "rgba(255,0,0,0.5)";
+        context.fillRect(collisionObjects[1].x,collisionObjects[1].y,collisionObjects[1].width,collisionObjects[1].height);
+        context.fillRect(collisionObjects[2].x,collisionObjects[2].y,collisionObjects[2].width,collisionObjects[2].height);
+
+        if(this.showHitBox) {
+            const playerCrouchDifference = playerCrouched&&!playerJumping?playerScale*3:0;
+            context.fillStyle = "rgba(0,0,255,0.5)";
+            context.fillRect(playerX-playerCollisionWidth/2,fullHeight-playerCollisionHeight-(playerY+jumpOffset)+playerCrouchDifference,playerCollisionWidth,playerCollisionHeight-playerCrouchDifference)
         }
 
         this.playerAnimator.render(timestamp,playerX,playerY+jumpOffset,playerScale);
